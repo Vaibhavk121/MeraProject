3c1dc3e671051ed853255494f7c7da250b03f6d6
Babithkp <babithkullachetti44@gmail.com>
2024-07-21 10:06:46 +0530

    stack is complete

--- src/app/stacks/page.tsx
+++ src/app/stacks/page.tsx
@@ -1,14 +1,13 @@
 
 import AboutStack from '@/components/BasicOparetion/stack/AboutStack'
 import AnimationLayout from '@/components/BasicOparetion/stack/AnimationLayout'
-import StackOperation from '@/components/BasicOparetion/stack/StackOperation'
-import Navbar from '@/components/nav-footer/Navbar'
+import SubNavbar from '@/components/nav-footer/SubNavbar'
 import React from 'react'
 
 const page = () => {
   return (
     <>
-      <Navbar/>
+      <SubNavbar/>
       <AboutStack/>
       <AnimationLayout/>
     </>
--- src/components/BasicOparetion/stack/AnimationLayout.tsx
+++ src/components/BasicOparetion/stack/AnimationLayout.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import { useGSAP } from "@gsap/react";
-import React, { useRef, useState } from "react";
-import { FaArrowRightLong } from "react-icons/fa6";
+import React, { useEffect, useRef, useState } from "react";
+import { FaArrowLeftLong, FaArrowRightLong } from "react-icons/fa6";
 import gsap from "gsap";
 gsap.registerPlugin(useGSAP);
 
@@ -12,136 +12,193 @@ interface Array {
 }
 
 const welomeText = `Please Click Any Operation to view the Arrays`;
-const boxClassVariable = "flex h-full items-center justify-center rounded-lg text-xl font-medium drop-shadow-lg bg-rose-500";
+const boxClassVariable =
+  "flex h-full items-center justify-center rounded-lg text-xl font-medium drop-shadow-lg bg-rose-500";
 
 const inputClass = "w-[5rem] h-[2.5rem] bg-transparent border rounded-md px-2";
 export default function AnimationLayout() {
-    const container = useRef<HTMLDivElement>(null);
-    const [createNumber, setCreateNumber] = useState(5);
-    const [pushElement, setPushElement] = useState(4)
-    const [mainArray, setMainArray] = useState<Array[]>([]);
-    const [sizeFullMsg, setSizeFullMsg] = useState(false)
-    const [defaultText, setDefaultText] = useState<boolean | string>(welomeText);
-    const [elementScanner, setElementScanner] = useState(false)
-    const [pushBtn,setPushBtn] = useState(false)
-    let [top, setTop] = useState(0)
-    let [topPointer, setTopPointer] = useState(1)
-    const { contextSafe } = useGSAP({ scope: container });
-  
+  const container = useRef<HTMLDivElement>(null);
+  const [createNumber, setCreateNumber] = useState(5);
+  const [pushElement, setPushElement] = useState(4);
+  const [mainArray, setMainArray] = useState<Array[]>([]);
+  const [Msg, setMsg] = useState<boolean | string>(false);
+  const [defaultText, setDefaultText] = useState<boolean | string>(welomeText);
+  const [elementScanner, setElementScanner] = useState(false);
+  const [pushBtn, setPushBtn] = useState(false);
+  let [top, setTop] = useState(0);
+  let [topPointer, setTopPointer] = useState(1);
+  const { contextSafe } = useGSAP({ scope: container });
+
   const arrayLength = mainArray.length;
   let timeline = gsap.timeline();
 
+  const createInputhander = (event: any) => {
+    event.preventDefault();
+    setCreateNumber(event.currentTarget.value);
+  };
+  const pushInputhander = (event: any) => {
+    event.preventDefault();
+    setPushElement(event.currentTarget.value);
+  };
 
-    const createInputhander = (event: any) => {
-      event.preventDefault();
-      setCreateNumber(event.currentTarget.value);
-    };
-    const pushInputhander = (event: any) => {
-      event.preventDefault();
-      setPushElement(event.currentTarget.value);
-    };
-
-    const generateDefaultArray = () => {
-      const newNumbers = [];
-      for (let i = 0; i < createNumber; i++) {
-        newNumbers.push({
-          id: i.toString(),
-          value: -1
-        });
-      }
-      setMainArray(newNumbers);
-      setDefaultText(false);
-    };
-
-
-    // Push opeartion to the timeline
-    const pushButtonHandler = ()=>{
-        const newArray = [...mainArray];
-        newArray[top] = {id: top.toString() ,value:pushElement}
-        setMainArray(newArray)
+  const generateDefaultArray = () => {
+    const newNumbers = [];
+    setTop(0);
+    for (let i = 0; i < createNumber; i++) {
+      newNumbers.push({
+        id: i.toString(),
+        value: -1,
+      });
     }
+    setMainArray(newNumbers);
+    setDefaultText(false);
+    setTimeout(() => {
+      gsap.fromTo(
+        ".scan",
+        { y: topPointer },
+        {
+          y: 0,
+          duration: 1,
+          ease: "back.inOut",
+        },
+      );
+    }, 10);
+    setTopPointer(-1);
+  };
 
-    
+  // Push opeartion to the timeline
+  const pushButtonHandler = () => {
+    const newArray = [...mainArray];
+    newArray[top] = { id: top.toString(), value: pushElement };
+    setMainArray(newArray);
+  };
 
-    const onPushOperation = contextSafe(()=>{
-      if(top >= arrayLength){
-        setSizeFullMsg(true)
-        setTimeout(()=>{
-          setSizeFullMsg(false)
-        },2000)
-        return
-      }
-        setElementScanner(true)
-        setTimeout(() => {
-            timeline.fromTo(`.box${top}`,
-                {y:-300,duration:1,ease:"back.inOut",opacity:0},
-                {y:0,duration:1,ease:"back.inOut",opacity:1}
-            )
-            
-        },1)
-        pushButtonHandler()
-        setTimeout(()=>{
-          setPushBtn(true)
-          const newLevel = topPointer + -65;
-          gsap.fromTo(".scan",  
-            { y: topPointer },
-            {
-              y: newLevel,
-              duration: 1,
-              ease: "back.inOut",
-              onComplete: () => {
-                setTopPointer(newLevel);
-                setPushBtn(false)
-              }
-          });
-          const newTop = top + 1;
-          setTop(newTop)
-        },1)
-    })
-
-    const popButtonHandler = ()=>{
-      popOperation()
-      setTimeout(()=>{
-        const newArray = [...mainArray];
-        newArray[top] = {id: top.toString() ,value:-1}
-        setMainArray(newArray)
-      },100)
+  const onPushOperation = contextSafe(() => {
+    if (top >= arrayLength) {
+      setMsg("Array size is full");
+      setTimeout(() => {
+        setMsg("");
+      }, 2000);
+      return;
     }
+    setElementScanner(true);
+    setTimeout(() => {
+      timeline.fromTo(
+        `.box${top}`,
+        { y: -300, duration: 1, ease: "back.inOut", opacity: 0 },
+        { y: 0, duration: 1, ease: "back.inOut", opacity: 1 },
+      );
+    }, 1);
+    pushButtonHandler();
+    setTimeout(() => {
+      setPushBtn(true);
+      const newLevel = topPointer + -65;
+      gsap.fromTo(
+        ".scan",
+        { y: topPointer },
+        {
+          y: newLevel,
+          duration: 1,
+          ease: "back.inOut",
+          onComplete: () => {
+            setTopPointer(newLevel);
+            setPushBtn(false);
+          },
+        },
+      );
+      const newTop = top + 1;
+      setTop(newTop);
+    }, 1);
+  });
 
-    const popOperation = () =>{
-      const newTop = top - 1;
-        setTop(newTop)
-      setTimeout(()=>{
-        timeline.fromTo(`.box${newTop}`,
-          {y:0,duration:1,ease:"back.inOut",opacity:1},
-          {y:-300,duration:1,ease:"back.inOut",opacity:0}
-        )
-      },1)
-       setTimeout(()=>{
-          const newLevel = topPointer - -65;
-          gsap.fromTo(".scan",  
-            { y: topPointer },
-            {
-              y: newLevel,
-              duration: 1,
-              ease: "back.inOut",
-              onComplete: () => {
-                setTopPointer(newLevel);
-              }
-          });
-        },1)
+  const popButtonHandler = () => {
+    if (top <= 0) {
+      setMsg("Nothing to POP");
+      setTimeout(() => {
+        setMsg(false);
+      }, 2000);
+      return;
+    }
+    const newTop = top - 1;
+    setTop(newTop);
+    setTimeout(() => {
+      timeline.fromTo(
+        `.box${newTop}`,
+        { y: 0, duration: 1, ease: "back.inOut", opacity: 1 },
+        { y: -300, duration: 1, ease: "back.inOut", opacity: 0 },
+      );
+    }, 1);
+    setTimeout(() => {
+      const newLevel = topPointer - -65;
+      gsap.fromTo(
+        ".scan",
+        { y: topPointer },
+        {
+          y: newLevel,
+          duration: 1,
+          ease: "back.inOut",
+          onComplete: () => {
+            setTopPointer(newLevel);
+          },
+        },
+      );
+    }, 1);
+    setTimeout(() => {
+      const newArray = [...mainArray];
+      newArray[newTop] = { id: newTop.toString(), value: -1 };
+      setMainArray(newArray);
+    }, 1000);
+  };
+
+  const onPeekBtnHandler = () => {
+    setTimeout(() => {
+      gsap.fromTo(
+        `.box${top - 1}`,
+        { scale: 1.5, duration: 2, ease: "back.inOut", backgroundColor: "red" },
+        {
+          scale: 1,
+          duration: 1,
+          ease: "back.inOut",
+          backgroundColor: "rgb(244 63 94 / var(--tw-bg-opacity))",
+        },
+      );
+    }, 1);
+  };
+
+  useEffect(() => {
+    let scalelength = 1;
+
+    if (arrayLength >= 8) {
+      scalelength = 0.7;
+    }
+    if (arrayLength >= 10) {
+      scalelength = 0.7;
+   
+    }
+    if (arrayLength >= 11) {
+      scalelength = 0.5;
+
+    }
+    if (arrayLength >= 16) {
+      scalelength = 0.3;
+    }
+    if (arrayLength >= 26) {
+      scalelength = 0.2;
+    }
+    setTimeout(() => {
+      gsap.fromTo(
+        `.stackContainer`,
+        {
+          scale: scalelength,
+          
+        },
+        {
+          scale: scalelength,
         
-    }
-
-    const onPeekBtnHandler = () =>{
-      setTimeout(()=>{
-        gsap.fromTo(`.box${top-1}`,
-          {scale:1.5,duration:2,ease:"back.inOut",backgroundColor:"red"},
-          {scale:1,duration:1,ease:"back.inOut",backgroundColor:"rgb(244 63 94 / var(--tw-bg-opacity))"})
-      },1)
-    }
-
-
+        },
+      );
+    }, 10);
+  }, [arrayLength]);
 
   return (
     <section
@@ -166,7 +223,7 @@ export default function AnimationLayout() {
                     >
                         Create
                     </Button>
-            </div>
+          </div>
           <div className="flex w-full items-center gap-3">
                 <p>Push Operation = </p>
                 <input
@@ -190,6 +247,7 @@ export default function AnimationLayout() {
                         className="w-[5rem] hover:scale-100 active:scale-95 duration-100 transition"
                         variant={"secondary"}
                         onClick={popButtonHandler}
+                        disabled={pushBtn ? true : false}
                     >
                         Pop()
                     </Button>
@@ -205,19 +263,19 @@ export default function AnimationLayout() {
                     </Button>
             </div>
         </div>
-        <div className="flex relative items-center justify-center flex-col-reverse p-2 w-full overflow-hidden corn">
-          {sizeFullMsg && <p className="text-2xl font-medium absolute bottom-5">Array size is Full</p>}
+        <div className="flex  relative items-center justify-center flex-col-reverse p-2 w-full overflow-hidden corn">
+          
         {defaultText && (
-              <span className="text-2xl font-medium">
+              <span className="text-2xl font-medium ">
                 {defaultText}
               </span>
             )}
-            <div className="flex  flex-col-reverse">
-            {elementScanner && <div className="scan absolute  p-1 rounded-md left-[30%] -bottom-5 w-[10rem] mb-[2rem] h-[5rem] flex items-center gap-1 text-xl">
-              Top<FaArrowRightLong size={30} />
-              </div>}
+            {!defaultText && <div className="flex  flex-col-reverse stackContainer">
+              <div className={`scan   p-1 rounded-md justify-end   w-[15rem] h-[5rem] flex items-center gap-1 text-xl ${elementScanner ? "" :"opacity-0"}`}>
+                <FaArrowLeftLong size={30} />Top
+              </div>
             {mainArray.map((ele, i) => (
-                <div key={i} className="">
+              <div key={i} className="">
                         <div className="border-2 border-white p-1  h-16 w-[10rem] flex flex-col justify-center ">
                             {ele.value !== -1 && <div className={boxClassVariable + " box " + "box" + ele.id}>
                                 {ele.value}
@@ -225,10 +283,10 @@ export default function AnimationLayout() {
                             </div>
                     </div>
                 ))}
+              </div>}
+              {Msg && <p className="text-2xl font-medium absolute  mb-3 bottom-5">{Msg}</p>}
               </div>
         </div>
-      </div>
     </section>
   );
 }
-
